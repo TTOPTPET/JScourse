7bcc6924f9788a253a1ae5e4d2b2732bcd28df0a
Maxim <ttoptpet@mail.ru>
2022-07-16 10:32:31 +0300

    if added

--- js/base.js
+++ js/base.js
@@ -89,3 +89,37 @@
 
     console.log(isChecked && !isClose);
     console.log(isChecked || isClose);
+
+    if (4 == 9) {
+        console.log('okay');
+    } else {
+        console.log('not okay');
+    }
+
+    const num = 50;
+
+    if (num < 49) {
+        console.log('error');
+    } else if (num > 100) {
+        console.log('too much');
+    } else {
+        console.log('okay');
+    }
+
+
+    (num == 50) ? console.log('OK') : console.log('error');  //тернарный оператор
+
+    switch (num) {
+        case 49: 
+            console.log('неверно');
+            break;
+        case 100:
+            console.log('неверно');
+            break;
+        case 50:
+            console.log('верно');
+            break;
+        default:
+            console.log('не повезло');
+            break;
+    }
